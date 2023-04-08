a1a93199ec08d5675d84f604a1da46c03d512940
Manoj Reddy <manureddy.g94@gmail.com>
2023-04-08 10:57:57 +0530

    Update Welcome.js

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -8,7 +8,7 @@ function Welcome() {
         <div className='landing'>
             <h1>WELCOME TO <span className='company'>EMTEKSOLUTIONS</span></h1>
             <p>
-                MTekSolutions is a US based IT development, services and products company that focuses on providing the best business integration service through the application of technology.
+                MTekSolutions is an US based IT development, services and products company that focuses on providing the best business integration service through the application of technology.
             </p>
             <Button variant="primary">
                 <Link to={'about'}>Read more</Link>
